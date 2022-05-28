11096ddc73ae3b9faf9211274b5458fd12f43a41
fgclue <schefferdaudtfelipe@gmail.com>
2022-05-27 22:22:24 -0300

    tes

--- i.hpp
+++ i.hpp
@@ -1,2 +1,11 @@
 #include <iostream>
 #include <unistd.h>
+
+/* 
+  TTTTTT II CCCCCC TTTTTT AAAAAA CCCCCC TTTTTT OOOOOO EEEEEE
+    TT   II CC       TT   AA__AA CC       TT   OO  OO EE--
+    TT   II CCCCCC   TT   AA  AA CCCCCC   TT   OOOOOO EEEEEE
+    in C++
+
+    Header File
+*/
