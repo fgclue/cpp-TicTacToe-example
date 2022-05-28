361b96f2ab4a42193fa915d81da8a5466aed280e
fgclue <schefferdaudtfelipe@gmail.com>
2022-05-27 22:17:54 -0300

    tired

--- main.cpp
+++ main.cpp
@@ -1,21 +1,31 @@
 #include "i.hpp"
 
 int main() {
-    using namespace std;
-    cout << "TicTacToe in C++ \n";
-    sleep(2);
-    cout << "Starting game... \n";
-    sleep(3);
-    cout << "Board \n";
-    cout << "O | O | O \n";
-    cout << "O | O | O \n";
-    cout << "O | O | O \n";
+    /* 
+      TTTTTT II CCCCCC TTTTTT AAAAAA CCCCCC TTTTTT OOOOOO EEEEEE
+        TT   II CC       TT   AA__AA CC       TT   OO  OO EE--
+        TT   II CCCCCC   TT   AA  AA CCCCCC   TT   OOOOOO EEEEEE
+        in C++
+    */
+    using namespace std; // Uses namespace std (for it being easier to use std::cout)
+    cout << "TicTacToe in C++ \n"; // Prints text to the screen
+    sleep(2); // Sleeps for 2 seconds
+    cout << "Starting game... \n"; // Prints text to the screen
+    sleep(3); // Sleeps for 3 seconds
+    cout << "Board \n"; // Prints text to the screen
+    cout << "O | O | O \n"; // Prints text to the screen
+    cout << "O | O | O \n"; // Prints text to the screen
+    cout << "O | O | O \n"; // Prints text to the screen
 
+    // thinkS
 
-    return 0;
+    return 0; // ?
 
-    //  Board
-    // X | O | X
-    // O | X | O
-    // X | O | X 
+    /*
+      Board
+    X | O | X
+    O | X | O
+    X | O | X 
+      Board
+    */
 }     
