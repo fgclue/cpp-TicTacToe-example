89eb86dd10ebeacb6bdcc93f97ab82ff9cf79815
fgclue <schefferdaudtfelipe@gmail.com>
2022-05-27 22:33:04 -0300

    sad

--- main.cpp
+++ main.cpp
@@ -6,18 +6,22 @@ int main() {
         TT   II CC       TT   AA__AA CC       TT   OO  OO EE--
         TT   II CCCCCC   TT   AA  AA CCCCCC   TT   OOOOOO EEEEEE
         in C++
+
+        Main Code
     */
     using namespace std; // Uses namespace std (for it being easier to use std::cout)
     cout << "TicTacToe in C++ \n"; // Prints text to the screen
     sleep(2); // Sleeps for 2 seconds
     cout << "Starting game... \n"; // Prints text to the screen
     sleep(3); // Sleeps for 3 seconds
+    /*
     cout << "Board \n"; // Prints text to the screen
     cout << "O | O | O \n"; // Prints text to the screen
     cout << "O | O | O \n"; // Prints text to the screen
     cout << "O | O | O \n"; // Prints text to the screen
+    */
 
-    // thinkS
+    // think
 
     return 0; // ?
 
