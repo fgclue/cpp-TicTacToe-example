8b2ea4eacaa281a26ce5f3c43b625368178e26b0
fgclue <schefferdaudtfelipe@gmail.com>
2022-05-27 21:50:45 -0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include "i.h"
+
+int main() {
+    using namespace std;
+    cout << "TicTacToe in C++ \n";
+    sleep(2); 
+    cout << "Starting game... \n";
+    sleep(3);
+    cout << "Board \n";
+    cout << "O | O | O \n";
+    cout << "O | O | O \n";
+    cout << "O | O | O \n";
+
+
+    return 0;
+
+    //  Board
+    // X | O | X
+    // O | X | O
+    // X | O | X 
+}     
